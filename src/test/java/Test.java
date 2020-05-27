2803e183863517c4d763cb4f006d78d4a0a64a48
vadymlos <vadym.los@pdffiller.team>
2020-05-27 22:31:30 +0300

    Third and new

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,4 +5,37 @@ public class Test {
     public void secondPush(){
         System.out.println("push");
     }
+    public void secondPush2(){
+        System.out.println("push222");
+    }
+    public void secondPush3(){
+        System.out.println("push");
+    }
+    public void secondPush4(){
+        System.out.println("push");
+    }
+    public void secondPush5(){
+        System.out.println("push");
+    }
+    public void secondPush6(){
+        System.out.println("push");
+    }
+
+    public void thirdPush(){
+        System.out.println("12");
+    }
+    public void thirdPush2(){
+        System.out.println("123");
+    }
+    public void thirdPush3(){
+        System.out.println("1234");
+    }
+    public void thirdPush4(){
+        System.out.println("12345");
+    }
+
+
+    public void thirdPush5(){
+        System.out.println("126");
+    }
 }
